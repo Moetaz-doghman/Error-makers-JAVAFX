6248d6b65c1ed2df9b980ada4047b62c925864ed
SkanderZouaoui <mohamedskander.zouaoui@esprit.tn>
2022-04-21 13:37:01 +0100

    LIST USERS

--- src/gui/ListUsersController.java
+++ src/gui/ListUsersController.java
@@ -4,23 +4,18 @@
  */
 package gui;
 
-import entities.Personne;
 import entities.User;
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import services.PersonneService;
+import javafx.scene.input.KeyEvent;
 import services.UserService;
 
 /**
@@ -28,9 +23,9 @@ import services.UserService;
  *
  * @author skanderzouaoui
  */
-public class AffichageController implements Initializable {
+public class ListUsersController implements Initializable {
 
-    @FXML
+     @FXML
     private TableView<User> tableview;
     @FXML
     private TableColumn<User, String> nom;
@@ -61,8 +56,11 @@ public class AffichageController implements Initializable {
         email.setCellValueFactory(new PropertyValueFactory<>("email"));
         telephone.setCellValueFactory(new PropertyValueFactory<>("telephone"));
         role.setCellValueFactory(new PropertyValueFactory<>("role"));
+    
     }    
 
-    
+    @FXML
+    private void rechercher(KeyEvent event) {
+    }
     
 }
